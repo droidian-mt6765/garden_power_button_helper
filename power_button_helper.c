a9c8c46b4170197016f1ce6aa8c5909659ced2cf
thatsitipl <83173962+thatsitipl@users.noreply.github.com>
2023-01-16 16:38:49 +0100

    Fix : Saving Brightness correctly

--- power_button_helper.c
+++ power_button_helper.c
@@ -4,71 +4,87 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdarg.h>
 
 // Convert seconds to micros
 #define SLEEP_PERIOD 0.5*1000000
 
 // For debug uncomment
 //#define DEBUG
+#define READ_FORMAT "Read : %d bytes from %s\n"
+#define WRITE_FORMAT "Wrote : %d bytes to %s\n"
+
+void debug(const char* format, ...) {
+	#ifdef DEBUG
+		va_list args;
+		va_start(args, format);
+		vprintf(format, args);
+		va_end(args);
+	#endif
+}
+
 
 int main(int argc,char* argv[]){
 	int backlight_stat_fd;
-	long backlight_stat = 0;
+	int backlight_stat = 0;
 	int brightness_control;
-	char readchar;
+	char read_char;
 	int last_stat;
 	char before_lock[4];
-	int read_bytes;
+
+
+	int bread;
+	int bread_before;
+	int bwritten;
 
 	backlight_stat_fd = open("/sys/android_brightness/brightness_light",O_RDONLY);
-	read(backlight_stat_fd,&readchar,1);
+	bread = read(backlight_stat_fd,&read_char,1);
 	close(backlight_stat_fd);
-	last_stat = readchar - '0';
 
-	while(true){
+	last_stat = read_char - '0';
 
+	for(;;) {
 		backlight_stat_fd = open("/sys/android_brightness/brightness_light",O_RDONLY);
-
-		read(backlight_stat_fd,&readchar,1);
-
+		int bread = read(backlight_stat_fd,&read_char,1);
 		close(backlight_stat_fd);
 
-		#ifdef DEBUG
-			printf("Read char : %c\n",readchar);
-		#endif
+		debug(READ_FORMAT,bread,"/sys/android_brightness/brightness_light");
+		debug("Read char : %c\n",read_char);
 
-		backlight_stat = readchar - '0';
+		backlight_stat = read_char - '0';
 
-		#ifdef DEBUG
-			printf("last_stat : %d, backlight_stat : %d\n",last_stat,backlight_stat);
-		#endif
+		debug("last_stat : %d, backlight_stat : %d\n",last_stat,backlight_stat);
 
 		if(backlight_stat == 5 && last_stat == 0){
 			//Screen Turned On
-
-			#ifdef DEBUG
-			puts("Changing Screen");
-			#endif
-
-			// Read the Brightness and save it
-			brightness_control = open("/sys/class/leds/lcd-backlight/brightness",O_RDONLY);
-			read_bytes = read(brightness_control,before_lock,4);
-			close(brightness_control);
-
-			#ifdef DEBUG
-			printf("Brightness before : %s\n",before_lock);
-			#endif
+			debug("Changing Screen\n");
+			debug("Brightness before : %s\n",before_lock);
 
 			// Setting to zero since the values must differ to turn on the screen
 			brightness_control = open("/sys/class/leds/lcd-backlight/brightness",O_WRONLY);
-			write(brightness_control,"0",1);
+			bwritten = write(brightness_control,"0",1);
 			close(brightness_control);
+
+			debug(WRITE_FORMAT,bwritten,"/sys/class/leds/lcd-backlight/brightness");
+			debug("Setting Brightness to %s\n",before_lock);
+
 			// Setting the Brightness saved before
 			brightness_control = open("/sys/class/leds/lcd-backlight/brightness",O_WRONLY);
-			write(brightness_control,before_lock,read_bytes);
-                        close(brightness_control);
-		}
+			bwritten = write(brightness_control,before_lock,bread_before);
+      close(brightness_control);
 
+			debug(WRITE_FORMAT,bwritten,"/sys/class/leds/lcd-backlight/brightness");
+
+		} else if(backlight_stat == 5 && last_stat == 5){
+			// Read the Brightness and save it
+			memset(before_lock, 0, 4);
+			brightness_control = open("/sys/class/leds/lcd-backlight/brightness",O_RDONLY);
+			bread_before = read(brightness_control,before_lock,4);
+			close(brightness_control);
+
+			debug(READ_FORMAT,bread,"/sys/class/leds/lcd-backlight/brightness");
+			debug("Current Brightness : %s\n",before_lock);
+		}
 		last_stat = backlight_stat;
 		usleep(SLEEP_PERIOD);
 	}
